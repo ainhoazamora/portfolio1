675969299898f849d71cc2dc8c437f5255bc2688
ainhoazamora <ainhoazamoram@gmail.com>
2026-08-23 19:38:11 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // GitHub Pages project site: https://ainhoazamora.github.io/portfolio/
 export default defineConfig({
   site: 'https://ainhoazamora.github.io',
-  base: '/portfolio',
+  base: '/portfolio1',
   trailingSlash: 'ignore',
   build: { format: 'directory' },
   compressHTML: true,
